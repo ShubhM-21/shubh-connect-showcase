a25427f14f1a65bb38b2b20eabc7469496f912e9
Shubh Madhyan <f20212703@goa.bits-pilani.ac.in>
2026-07-09 10:25:06 +0530

    Feat: Updating the resume

--- src/lib/siteConfig.ts
+++ src/lib/siteConfig.ts
@@ -1,3 +1,3 @@
 // Centralized configuration file for portfolio website
 export const RESUME_PATH =
-  "https://drive.google.com/uc?export=download&id=1OBBFYFDmX1dWhYqO5Em-r9-DHONeov2r";
+  "https://drive.google.com/uc?export=download&id=163-1L71RpsBOPW70IcBi868KrhSwQ6ik";
