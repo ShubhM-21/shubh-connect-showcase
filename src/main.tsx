6ebad564d954d5c8b7d82831be1dd865ae6819b3
Shubh Madhyan <f20212703@goa.bits-pilani.ac.in>
2026-07-09 09:16:12 +0530

    Feat: integrate vercel web analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Analytics } from "@vercel/analytics/react" // 1. Added import
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <Analytics /> {/* 2. Added component */}
+  </>
+);
